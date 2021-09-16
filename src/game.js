827b94f39a5e1bf49ffef8da2d3d6c2936d2c696
nagaveninaik <70188382+nagaveninaik@users.noreply.github.com>
2021-09-16 09:32:05 +0530

    Add files via upload

--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
-import Phaser from 'phaser';
-//import MyGame from 'MyGame.js';
+import Phaser, { Scene } from 'phaser';
 import boy from './assets/boy.png';
 import numberLine from './assets/numberLine.png';
+
 
 class MyGame extends Phaser.Scene
 {
@@ -12,28 +12,50 @@ class MyGame extends Phaser.Scene
 
     preload ()
     {
-    //    ' this.load.image('logo', logoImg);
         this.load.image('numberLine',numberLine);
         this.load.image('boy',boy);
     }
-      
+   
+       
     create ()
     {
-        //const logo = this.add.image(400, 150, 'logo');
+        
         this.numberLine = this.add.image(375,850,'numberLine');
         this.numberLine.setOrigin(0.5);
         this.boy = this.add.image(375,700,'boy');
         
-      
-        this.tweens.add({
-            targets: this.boy,
-            x: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
+        // console.log(this.boy)
+        let Boy = this.boy
+        let that = this
+   
+       
+        this.boy.setInteractive().on("pointerdown", function (pointer, localX, localY, event) {
+            // console.log(Boy)
+            // console.log(this.boy)
+            let move =  that.tweens.add({
+                targets: Boy,
+                x: 450,
+                duration: 1000,
+                ease: "Power2",
+                yoyo: true,
+                loop: 1,
+                
+            });
+        
+        
         });
-    }
+	
+		
+        
+		
+
+        
+    
 }
+}
+function update() {
+    
+}
+
 
 export default MyGame;
