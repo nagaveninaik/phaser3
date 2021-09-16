206366b5c7877e8646b6f7b5e4361c1f0e84e47f
nagaveninaik <70188382+nagaveninaik@users.noreply.github.com>
2021-09-16 15:56:30 +0530

    set the boy tween range -2 to 2

--- src/game.js
+++ src/game.js
@@ -1,4 +1,4 @@
-import Phaser, { Scene } from 'phaser';
+import Phaser from 'phaser';
 import boy from './assets/boy.png';
 import numberLine from './assets/numberLine.png';
 
@@ -23,42 +23,42 @@ class MyGame extends Phaser.Scene
         this.numberLine = this.add.image(375,850,'numberLine');
         this.numberLine.setOrigin(0.5);
         this.boy = this.add.image(375,700,'boy');
-        
+        this.x =2;
+        this.y=3;
+        this.z=this.x*this.y;
+        // this.z = x*y;
+         //this.z=x*
+        this.add.text(10, 10, `${this.x}+${this.y}= ${this.z}`, { fontFamily: 'serif',fontSize: '30px' });
         // console.log(this.boy)
-        let Boy = this.boy;
-        let that = this;
-        
+        // let Boy = this.boy;
+        // let that = this;
+
    
        
-        this.boy.setInteractive().on("pointerdown", function (pointer, localX, localY, event) {
-            // console.log(Boy)
-            // console.log(this.boy)
-           
-            let move =  that.tweens.add({
-                targets: Boy,
-                x: Math.random(),
-                //y: Math.random(),
-                duration: 1000,
-                ease: "Power2",
-                yoyo: true,
-                loop: 1,
-                
-            });
+        this.boy.setInteractive().on("pointerdown", (pointer, localX, localY, event)=>
+        this.moveBoyTween()
+        );
+    
+}
+moveBoyTween()
+{
+    // console.log(Boy)
+    // console.log(this.boy)
+   
+     this.tweens.add({
+        targets: this.boy,
+        x: Phaser.Math.Between(187.5, 562.5),
+        //y: Math.random(),
+        duration: 1000,
+        ease: "Power2",
         
         
-        });
-	
-		
         
-		
+    });
+}
 
-        
-    
 }
-}
-function update() {
-    
-}
+
 
 
 export default MyGame;
